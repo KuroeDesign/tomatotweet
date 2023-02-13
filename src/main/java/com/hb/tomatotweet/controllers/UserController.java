ba413f323076e703a5bcc2e5be52d4fcbce501fd
Dufau-Sansot Cloé <cloe.ds@hotmail.com>
2023-02-13 17:34:03 +0100

    Ajout d'un article

--- src/main/java/com/hb/tomatotweet/controllers/UserController.java
+++ src/main/java/com/hb/tomatotweet/controllers/UserController.java
@@ -1,13 +1,16 @@
 package com.hb.tomatotweet.controllers;
 
+import com.hb.tomatotweet.dtos.CategoryDTO;
 import com.hb.tomatotweet.dtos.UserDTO;
 import com.hb.tomatotweet.dtos.UserFormDTO;
+import com.hb.tomatotweet.services.CategoryService;
 import com.hb.tomatotweet.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,15 +18,16 @@ import java.util.List;
 public class UserController {
 
 	private UserService userService;
+	private CategoryService categoryService;
 	
 	public UserController(UserService userService) {
-		this.userService = userService;		
+		this.userService = userService;
 	}
 	
 	@GetMapping("private/user")
 	public ModelAndView adminPage() {		
 		List<UserDTO> users = userService.getUsers();
-		ModelAndView mav = new ModelAndView("home-user");
+		ModelAndView mav = new ModelAndView("home");
 		mav.addObject("users", users);		
 		return mav;
 	}
@@ -39,6 +43,21 @@ public class UserController {
 	public ModelAndView registerUser(@ModelAttribute UserFormDTO user) {		
 		userService.saveUser(user);		
 		ModelAndView mav = new ModelAndView("redirect:/login");
+		return mav;		
+	}
+	
+	@GetMapping("private/user/addcategories")
+	public ModelAndView getAddCategoriesForm( CategoryService categoryService) {
+		List<CategoryDTO> categories = categoryService.getCategories();
+		ModelAndView mav = new ModelAndView("addcategories");
+		mav.addObject("categorie", categories);
+		return mav;
+	}
+	
+	@PostMapping("private/user/addcategories")
+	public ModelAndView selectCategories(@RequestParam  Object cat,  CategoryService categoryService) {		
+		System.out.println(cat);
+		ModelAndView mav = new ModelAndView("redirect:/");
 		return mav;		
 	}
 	
