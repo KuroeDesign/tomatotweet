67f4112c14985930be69f21339360bba4a849bf3
Dufau-Sansot Cloé <cloe.ds@hotmail.com>
2023-02-13 13:48:09 +0100

    tomatotweet first commit

---
+++ src/main/java/com/hb/tomatotweet/TomatotweetApplication.java
@@ -0,0 +1,13 @@
+package com.hb.tomatotweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatotweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatotweetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hb/tomatotweet/TomatotweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.tomatotweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomatotweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
